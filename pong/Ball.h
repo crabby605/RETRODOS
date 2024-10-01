db6bcee2951f1b8245625c24e5aa211c31605df8
crabby605 <128237026+crabby605@users.noreply.github.com>
2024-10-01 15:02:00 +0530

    Create Ball.h

---
+++ pong/Ball.h
@@ -0,0 +1,31 @@
+#ifndef PONG_BALL_H
+#define PONG_BALL_H
+
+#include <ncurses.h>
+
+class Ball {
+public:
+    Ball(double x, double y, int speed);
+    double getX() { return x; }
+    double getY() { return y; }
+    int getSpeed() { return speed; }
+    void setX(double a) {
+        x = a;
+    }
+    void setY(double a) {
+        y = a;
+    }
+    void setSpeed(int a){
+        speed = a;
+    }
+    void drawBall(double x, double y) {
+        mvaddch(x, y, 'o');
+    }
+private:
+    double x, y;
+    int speed;
+
+};
+
+
+#endif //PONG_BALL_H
